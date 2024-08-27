26533592f90050479fc60e32751143e3b92ac182
AlessioMartella7 <alessio.mart87@gmail.com>
2024-08-27 22:33:50 +0200

    esercizio terminato

--- js/script.js
+++ js/script.js
@@ -1,16 +1,3 @@
-/* Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
-Dopo 30 secondi i numeri scompaiono e appaiono invece 5 input in cui l'utente deve inserire i numeri che ha visto precedentemente, nell'ordine che preferisce.
-Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
-NOTA: non è importante l'ordine con cui l'utente inserisce i numeri, basta che ne indovini il più possibile.
-BONUS 1:
-Inseriamo la validazione: se l'utente mette due numeri uguali o inserisce cose diverse da numeri lo blocchiamo in qualche modo.
-BONUS 2:
-Generiamo gli input da JS, invece di scriverli nel codice
-Consigli del giorno:
-* Pensate prima in italiano.
-* Dividete in piccoli problemi la consegna.
-* Individuate gli elementi di cui avete bisogno per realizzare il programma.
-* Immaginate la logica come fosse uno snack: "Dati 2 array di numeri, indica quali e quanti numeri ci sono in comune tra i due array" */
 
 // recuperiamo dal DOM gli elementi di cui abbiamo bisogno
 // creiamo un bottone alla quale aggiungere degli eventi al click:
@@ -102,9 +89,6 @@ playButton.addEventListener('click', () => {
         form.classList.remove('d-none');
     }
     },1000 );
-
-    // array numeri controllati
-   
 
     // aggiungiamo un evento al form
     form.addEventListener ('submit', event => {
